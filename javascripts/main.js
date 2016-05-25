8a5fd4f4c716215ddc7a4ec0ec750a89cc040e00
wellingtondellamura <della.mura@gmail.com>
2016-05-25 13:49:44 -0300

    Recall Site

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 function gen_mail_to_link(lhs,rhs,subject){
-   document.write(“<AEF=\”mailto”);
-   document.write(“:” + lhs + “@”);
-   document.write(rhs + “?subject=” + subject + “\”>” + lhs + “@” + rhs + “<\/A>”);
+   document.write("<AEF=\"mailto");
+   document.write(":" + lhs + "@");
+   document.write(rhs + "?subject=" + subject + "\">" + lhs + "@" + rhs + "<\/A>");
 }
